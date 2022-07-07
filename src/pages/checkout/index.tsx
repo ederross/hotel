0030adf88f2710249dbf9102f489c9bceb26a223
EderJunior07 <eder_junior07@hotmail.com>
2022-07-06 23:42:48 -0300

    fix(checkout)

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -4,12 +4,13 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChevronLeft, ChevronDown, ChevronUp } from 'react-feather';
 import { CheckoutSucessModal } from '../../components/CheckoutSucessModal';
 import Footer from '../../components/common/Footer';
 import Input from '../../components/common/Input';
 import Header from '../../components/Header';
+import { useWindowSize } from '../../hooks/UseWindowSize';
 import { currency } from '../../utils/currency';
 
 import styles from './styles.module.scss';
@@ -17,6 +18,9 @@ import styles from './styles.module.scss';
 const Checkout = ({ officeDetails, design }: any) => {
   const { t } = useTranslation();
 
+  // Window Sizes
+  const size = useWindowSize();
+  const [scrolled, setScrolled] = useState(false);
   const router = useRouter();
   const [policy, setPolicy] = useState(0);
   const [successModalVisible, setSuccessModalVisible] = useState(false);
@@ -30,6 +34,19 @@ const Checkout = ({ officeDetails, design }: any) => {
     setSuccessModalVisible(!successModalVisible);
   };
 
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
     <>
       <Head>
@@ -37,7 +54,12 @@ const Checkout = ({ officeDetails, design }: any) => {
       </Head>
       <Header design={design} />
       <main className={styles.mainBox}>
-        <div className={styles.mobHeader}>
+        <div
+          className={styles.mobHeader}
+          style={{
+            boxShadow: scrolled ? '0px 1px 7px -2px #C8C8C8' : null,
+          }}
+        >
           <div onClick={() => router.back()} className={styles.btnGoBack}>
             <ChevronLeft width={24} height={24} />
           </div>
@@ -50,7 +72,7 @@ const Checkout = ({ officeDetails, design }: any) => {
             <h4>Total(BRL)</h4>
           </div>
           <div>
-            <h3>R$ 98,00</h3>
+            <h3>R$ 148,00</h3>
           </div>
         </div>
 
@@ -66,28 +88,116 @@ const Checkout = ({ officeDetails, design }: any) => {
 
               <div className={styles.content}>
                 <div className={styles.infoBox}>
-                  <h3>Sua hospedagem</h3>
+                  <div
+                    style={{
+                      borderBottom: size.width < 868 && '8px solid #dadada',
+                      padding: size.width < 868 && '0 1rem',
+                      paddingBottom: size.width < 868 && '1rem',
+                    }}
+                  >
+                    {size.width < 868 &&
+                      rooms.slice(0, 2).map((room, index) => (
+                        <>
+                          <div key={index} className={styles.roomContainer}>
+                            <div className={styles.imageRoomHolder}>
+                              <Image
+                                src={
+                                  index === 1
+                                    ? 'https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+                                    : index == 2
+                                    ? 'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                                    : 'https://images.unsplash.com/photo-1574643156929-51fa098b0394?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                                }
+                                layout={'fill'}
+                                width={124}
+                                height={124}
+                              />
+                            </div>
 
-                  <div className={styles.infoHolder}>
-                    <div>
-                      <h4>Datas</h4>
-                      <h5>23 - 29 de mai.</h5>
-                    </div>
-                    <div>
-                      <h4>Total Hóspedes</h4>
-                      <h5>12</h5>
+                            <div className={styles.roomInfo}>
+                              <div
+                                className={styles.roomNameAdultChildContainer}
+                              >
+                                <h5>
+                                  {room.adults}{' '}
+                                  {t('adult', { count: room.adults })} {'&'}{' '}
+                                  {room.adults}{' '}
+                                  {t('children', { count: room.children })}
+                                </h5>
+                                <h4>{room.objectName}</h4>
+                              </div>
+
+                              <div className={styles.roomQtndPriceContainer}>
+                                <h5>{room.quantity + ' ' + t('room')} </h5>
+                                <h4>{currency(room.price)}</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      ))}
+                    {size.width < 868 && rooms.length > 2 && (
+                      <div
+                        style={{ marginTop: '1.5rem' }}
+                        className={styles.buttonSeeMoreRoomsContainer}
+                      >
+                        <button>Ver todos</button>
+                      </div>
+                    )}
+                    {size.width < 868 && (
+                      <div
+                        className={styles.divisorContainer}
+                        style={{ marginTop: '1rem' }}
+                      >
+                        <div></div>
+                      </div>
+                    )}
+
+                    <div style={{ marginTop: '20px' }}>
+                      <h3>Serviços</h3>
+                      {services.map((room, index) => (
+                        <div key={index} className={styles.roomContainer}>
+                          <div className={styles.row}>
+                            <h4>{room.objectName}</h4>
+                            <div
+                              style={{
+                                display: 'flex',
+                                alignItems: 'flex-end',
+                                flexDirection: 'column',
+                              }}
+                            >
+                              <h5>x{room.quantity}</h5>
+                              <h5>{currency(room.price)}</h5>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
 
-                  <div
-                    className={styles.infoHolder}
-                    style={{ flexDirection: 'column' }}
-                  >
-                    <h4>{t('arrivalForecast')}</h4>
-                    <div className={styles.cSelect}>
-                      <select name="arrivalForecast" id="pet-select">
-                        <option value="">12h - 18h</option>
-                      </select>
+                  <div style={{ padding: '0 1rem', paddingTop: '1rem' }}>
+                    <h3>Sua hospedagem</h3>
+
+                    <div className={styles.infoHolder}>
+                      <div>
+                        <h4>Datas</h4>
+                        <h5>23 - 29 de mai.</h5>
+                      </div>
+                      <div>
+                        <h4>Total Hóspedes</h4>
+                        <h5>12</h5>
+                      </div>
+                    </div>
+
+                    <div
+                      className={styles.infoHolder}
+                      style={{ flexDirection: 'column' }}
+                    >
+                      <h4>{t('arrivalForecast')}</h4>
+                      <div className={styles.cSelect}>
+                        <select name="arrivalForecast" id="pet-select">
+                          <option value="">12h - 18h</option>
+                        </select>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -98,39 +208,46 @@ const Checkout = ({ officeDetails, design }: any) => {
               >
                 <div></div>
               </div>
-              <div className={styles.mobInfoPriceContainer}>
-                <h3>Informações de preço</h3>
-                <div className={styles.mobInfoHolder}>
-                  <div>
-                    <u>
-                      {' '}
-                      <h5>Hospedagem 18 noites</h5>
-                    </u>
-                  </div>
-                  <div>
-                    <u>
-                      {' '}
-                      <h5>R$98,00</h5>
-                    </u>
-                  </div>
-                </div>
 
-                <div className={styles.mobInfoTotalHolder}>
-                  <div>
-                    <h5>Total(BRL)</h5>
-                  </div>
-                  <div>
-                    <h5>R$198,00</h5>
-                  </div>
+              <div className={styles.mobPriceInformation}>
+                <h4>Informações de preço</h4>
+                <div className={styles.row}>
+                  <h5>R$1.071 x 8 noites</h5>
+                  <h5>{currency(8574.72)}</h5>
+                </div>
+                <div className={styles.row}>
+                  <u>
+                    <h5>Serviços</h5>
+                  </u>
+                  <h5>{currency(98)}</h5>
+                </div>
+                <div className={styles.row}>
+                  <u>
+                    <h5>Taxas</h5>
+                  </u>
+                  <h5>{currency(98)}</h5>
+                </div>
+                <div className={styles.row}>
+                  <u>
+                    <h5>Impostos</h5>
+                  </u>
+                  <h5>{currency(98)}</h5>
                 </div>
 
                 <div className={styles.mobMoreInfoHolder}>
                   <u>
-                    <h5 style={{ alignSelf: 'center' }}>Mais informações</h5>
+                    <h5>Mais informações</h5>
                   </u>
                 </div>
               </div>
-
+              {size.width > 868 && (
+                <div
+                  className={styles.divisorContainer}
+                  style={{ padding: '0 1rem' }}
+                >
+                  <div></div>
+                </div>
+              )}
               <div className={styles.mobPersonalDataContainer}>
                 <h3>Dados pessoais</h3>
 
@@ -248,53 +365,46 @@ const Checkout = ({ officeDetails, design }: any) => {
             </div>
             <div className={styles.webPaymentInfos}>
               <div>
-                {rooms.length < 4 &&
-                  rooms.map((room, index) => (
-                    <div key={index} className={styles.roomContainer}>
-                      <div className={styles.imageRoomHolder}>
-                        <Image
-                          src={
-                            index === 1
-                              ? 'https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
-                              : index == 2
-                              ? 'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                              : 'https://images.unsplash.com/photo-1574643156929-51fa098b0394?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                          }
-                          layout={'fill'}
-                          width={124}
-                          height={124}
-                        />
-                      </div>
-
-                      <div className={styles.roomInfo}>
-                        <div className={styles.roomNameAdultChildContainer}>
-                          <h5>
-                            {room.adults} {t('adult', { count: room.adults })}{' '}
-                            {'&'} {room.adults}{' '}
-                            {t('children', { count: room.children })}
-                          </h5>
-                          <h4>{room.objectName}</h4>
-                        </div>
-
-                        <div className={styles.roomQtndPriceContainer}>
-                          <h5>{room.quantity + ' ' + t('room')} </h5>
-                          <h4>{currency(room.price)}</h4>
-                        </div>
-                      </div>
-                      {/* <div className={styles.row}>
-                      <h4>{room.objectName}</h4>
-                      <h5>x{room.quantity}</h5>
+                {rooms.slice(0, 2).map((room, index) => (
+                  <div key={index} className={styles.roomContainer}>
+                    <div className={styles.imageRoomHolder}>
+                      <Image
+                        src={
+                          index === 1
+                            ? 'https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+                            : index == 2
+                            ? 'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                            : 'https://images.unsplash.com/photo-1574643156929-51fa098b0394?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                        }
+                        layout={'fill'}
+                        width={124}
+                        height={124}
+                      />
                     </div>
 
-                    <div className={styles.row}>
-                      <h6>
-                        {room.adults} {t('adult', { count: room.adults })} {'&'}{' '}
-                        {room.adults} {t('children', { count: room.children })}
-                      </h6>
-                      <h5>{currency(room.price)}</h5>
-                    </div> */}
+                    <div className={styles.roomInfo}>
+                      <div className={styles.roomNameAdultChildContainer}>
+                        <h5>
+                          {room.adults} {t('adult', { count: room.adults })}{' '}
+                          {'&'} {room.adults}{' '}
+                          {t('children', { count: room.children })}
+                        </h5>
+                        <h4>{room.objectName}</h4>
+                      </div>
+
+                      <div className={styles.roomQtndPriceContainer}>
+                        <h5>{room.quantity + ' ' + t('room')} </h5>
+                        <h4>{currency(room.price)}</h4>
+                      </div>
                     </div>
-                  ))}
+                  </div>
+                ))}
+
+                {rooms.length > 2 && (
+                  <div className={styles.buttonSeeMoreRoomsContainer}>
+                    <button>Ver todos</button>
+                  </div>
+                )}
 
                 <div style={{ marginTop: '20px' }}>
                   <h3>Serviços</h3>
@@ -361,6 +471,20 @@ const Checkout = ({ officeDetails, design }: any) => {
                 >
                   Confirmar
                 </button>
+
+                <div className={styles.termsArea}>
+                  <h6>
+                    Ao clicar no botão abaixo, concordo com as seguintes
+                    políticas:{' '}
+                    <strong>
+                      {' '}
+                      <u>
+                        Políticas de Reserva, Política de Reembolso e Remarcação
+                        de Reserva.
+                      </u>
+                    </strong>
+                  </h6>
+                </div>
               </div>
             </div>
           </div>
@@ -382,7 +506,6 @@ const Checkout = ({ officeDetails, design }: any) => {
             <div className={styles.policyCardContainer}>
               <div className={styles.policyCard}>
                 <h3>Regras da casa</h3>
-                
               </div>
               <div className={styles.policyCard}>
                 <h3>Saúde {'&'} segurança</h3>
@@ -432,9 +555,17 @@ const Checkout = ({ officeDetails, design }: any) => {
                 <div className={styles.row}>
                   <h3>Regras da casa</h3>
                   {policy === 0 ? (
-                    <ChevronUp className={styles.chevronIcon} width={18} height={18} />
+                    <ChevronUp
+                      className={styles.chevronIcon}
+                      width={18}
+                      height={18}
+                    />
                   ) : (
-                    <ChevronDown className={styles.chevronIcon}  width={18} height={18} />
+                    <ChevronDown
+                      className={styles.chevronIcon}
+                      width={18}
+                      height={18}
+                    />
                   )}
                 </div>
                 {policy === 0 && (
@@ -450,9 +581,17 @@ const Checkout = ({ officeDetails, design }: any) => {
                 <div className={styles.row}>
                   <h3>Saúde {'&'} segurança</h3>
                   {policy === 1 ? (
-                    <ChevronUp className={styles.chevronIcon}  width={18} height={18} />
+                    <ChevronUp
+                      className={styles.chevronIcon}
+                      width={18}
+                      height={18}
+                    />
                   ) : (
-                    <ChevronDown className={styles.chevronIcon}  width={18} height={18} />
+                    <ChevronDown
+                      className={styles.chevronIcon}
+                      width={18}
+                      height={18}
+                    />
                   )}
                 </div>
                 {policy === 1 && (
@@ -468,9 +607,17 @@ const Checkout = ({ officeDetails, design }: any) => {
                 <div className={styles.row}>
                   <h3>Política de cancelamento</h3>
                   {policy === 2 ? (
-                    <ChevronUp className={styles.chevronIcon}  width={18} height={18} />
+                    <ChevronUp
+                      className={styles.chevronIcon}
+                      width={18}
+                      height={18}
+                    />
                   ) : (
-                    <ChevronDown className={styles.chevronIcon}  width={18} height={18} />
+                    <ChevronDown
+                      className={styles.chevronIcon}
+                      width={18}
+                      height={18}
+                    />
                   )}
                 </div>
                 {policy === 2 && (
