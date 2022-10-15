54344b250801a49cf9aaef116201274913dead79
Luiz Carlos Rodrigues <luizcarlos_rodrigues@yahoo.com.br>
2022-10-15 13:03:41 -0300

    SSL Error fix Axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,14 @@ import axios from 'axios';
 export const dynamicOffice = true;
 export const officeId = 'office1';
 export const baseURL = 'https://book.hospeda.in';
+const https = require('https');
+
 
 const api = axios.create({
   baseURL,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
 });
 
 export default api;
