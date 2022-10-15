efa676b8b8215c070026a55e6b6552b320f3a900
Luiz Carlos Rodrigues <luizcarlos_rodrigues@yahoo.com.br>
2022-10-15 11:14:37 -0300

    Dynamic Office Enabled

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const dynamicOffice = false;
+export const dynamicOffice = true;
 export const officeId = 'office1';
 export const baseURL = 'https://book.hospeda.in';
 
