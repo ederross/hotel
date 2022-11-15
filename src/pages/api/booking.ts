c555e3f85596134908b5acf6fc305a6a1ab35002
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2022-11-15 10:43:39 -0300

    fix(checkout): error message

--- src/pages/api/booking.ts
+++ src/pages/api/booking.ts
@@ -7,19 +7,22 @@ export default function handler(req, res) {
     //officeID
     const xfowardedHost = req.headers['x-forwarded-host'];
     console.log('X-fowardedHost: ' + xfowardedHost);
-  
+
     const id =
       dynamicOffice && !!xfowardedHost
         ? xfowardedHost?.toString()?.split('.')[0]
         : officeId;
-
-    PostBooking(req?.body?.cart, req?.body?.client, req?.body?.payment)
+    PostBooking(
+      { ...req?.body?.cart, officeId: id },
+      req?.body?.client,
+      req?.body?.payment
+    )
       .then((data: any) => {
-        res.status(200).json(data);
+        res.status(200).json(data?.data);
       })
       .catch((err) => {
         console.log('>>> FALHA AO RESERVAR QUARTO! <<<\n', err);
-        res.status(200).json({});
+        res.status(err?.status || 500).json(err);
       });
   } catch (error) {
     res.status(500).json('Post Booking Error');
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const apiToken = process.env.NEXT_PUBLIC_API_TOKEN;
-export const credentials = Buffer.from('' + ':' + apiToken).toString('base64'); 
-export const dynamicOffice = process.env.NEXT_PUBLIC_DYNAMIC_OFFICE === "true" ? true : false;
-export const officeId = 'office1';
+export const credentials = Buffer.from('' + ':' + apiToken).toString('base64');
+export const dynamicOffice =
+  process.env.NEXT_PUBLIC_DYNAMIC_OFFICE === 'true' ? true : false;
+export const officeId = 'centralparque';
 export const baseURL = process.env.NEXT_PUBLIC_BASE_URL; //'http://book.hospeda.in';
 const https = require('https');
 
